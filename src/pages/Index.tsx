a0feac7585a47f9a4e9ef3cd4c40139042ee51e4
[dyad] <gamermonoufia2@gmail.com>
2025-07-23 00:27:39 +0300

    Reverted all changes back to version 585df35316ba92fbdd930b2ac68179c9d5599992

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,20 @@
-import ContactSection from "@/components/ContactSection";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import LatestWriting from "@/components/LatestWriting";
+import Footer from "@/components/Footer";
+import ScrollingBlogs from "@/components/ScrollingBlogs";
 
 const Index = () => {
   return (
-    <ContactSection />
+    <div className="bg-background text-foreground min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <LatestWriting />
+        <ScrollingBlogs />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
